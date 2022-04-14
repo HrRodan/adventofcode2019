8c62a83ddc9d5815ff99bda6a7143ad395b296e3
HrRodan <m.mueller89@gmail.com>
2022-04-14 21:57:50 +0200

    day2

--- day02/day02.py
+++ day02/day02.py
@@ -10,11 +10,12 @@ def run_program(program: List[int], p1: int, p2: int):
     program_final[1] = p1
     program_final[2] = p2
     for i in range(0, len(program_final), 4):
-        if program_final[i] == 99:
+        opcode = program_final[i]
+        if opcode == 99:
             break
-        opcode, position_1, position_2, postion_out = program_final[i: i + 4]
         if opcode not in [1, 2]:
             raise Exception('Something went wrong!')
+        position_1, position_2, postion_out = program_final[i + 1: i + 4]
         value1 = program_final[position_1]
         value2 = program_final[position_2]
         result = value1 + value2 if opcode == 1 else value1 * value2
@@ -26,7 +27,7 @@ def run_program(program: List[int], p1: int, p2: int):
 print(run_program(program_start, 12, 2))
 
 # part2
-for i, j in product(*[range(100)]*2):
+for i, j in product(*[range(100)] * 2):
     if run_program(program_start, i, j) == 19690720:
         print(100 * i + j)
         break
