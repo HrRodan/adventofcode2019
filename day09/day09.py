75c2c61bcc880302f4425a9094e7750520c178f4
HrRodan <m.mueller89@gmail.com>
2022-04-19 22:30:30 +0200

    day9

--- day09/day09.py
+++ day09/day09.py
@@ -21,6 +21,10 @@ def get_value(mode: str, p_value: int, program: Dict[int, int], relative_base):
         raise ValueError('Something went wrong!')
 
 
+def get_set_position(mode: str, out: int, relative_base: int):
+    return out if mode != '2' else out + relative_base
+
+
 def run_program(program: Dict[int, int], p_input: Iterator[int]):
     program_final = program.copy()
     output = []
@@ -36,13 +40,13 @@ def run_program(program: Dict[int, int], p_input: Iterator[int]):
             p1, p2, out = [program_final[x] for x in range(i + 1, i + 4)]
             value1 = get_value(mode[-1], p1, program_final, relative_base)
             value2 = get_value(mode[-2], p2, program_final, relative_base)
-            out_value = out if mode[-3] != '2' else out + relative_base
+            out_value = get_set_position(mode[-3], out, relative_base)
             result = value1 + value2 if opcode == 1 else value1 * value2
             program_final[out_value] = result
             i += 4
         elif opcode == 3:
             out = program_final[i + 1]
-            out_value = out if mode[-1] != '2' else out + relative_base
+            out_value = get_set_position(mode[-1], out, relative_base)
             program_final[out_value] = next(p_input)
             i += 2
         elif opcode == 4:
@@ -62,7 +66,7 @@ def run_program(program: Dict[int, int], p_input: Iterator[int]):
             p1, p2, out = [program_final[x] for x in range(i + 1, i + 4)]
             value1 = get_value(mode[-1], p1, program_final, relative_base)
             value2 = get_value(mode[-2], p2, program_final, relative_base)
-            out_value = out if mode[-3] != '2' else out + relative_base
+            out_value = get_set_position(mode[-3], out, relative_base)
             if (opcode == 7 and value1 < value2) or (opcode == 8 and value1 == value2):
                 program_final[out_value] = 1
             else:
@@ -77,8 +81,9 @@ def run_program(program: Dict[int, int], p_input: Iterator[int]):
 
     return output
 
-#part 1
+
+# part 1
 print(run_program(program_start_dict, iter([1]))[0])
 
-#part 2
+# part 2
 print(run_program(program_start_dict, iter([2]))[0])
