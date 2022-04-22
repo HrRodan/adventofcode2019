d1d3c78528c3e1af1433d31df2d36abbd610d63d
HrRodan <m.mueller89@gmail.com>
2022-04-22 23:20:01 +0200

    day12

--- day12/day12.py
+++ day12/day12.py
@@ -6,7 +6,7 @@ with open('input.txt') as file:
     positions = np.array([[int(x) for x in re.findall(r'-?\d+', s.strip())] for s in file.readlines()]).astype(np.short)
 
 start = positions.copy()
-velocity = np.zeros(positions.shape, dtype=np.byte)
+velocity = np.zeros(positions.shape, dtype=np.short)
 velocity_start = velocity.copy()
 steps_part1 = 1000
 
@@ -19,7 +19,7 @@ while True:
         velocity = velocity + np.where(np.equal(positions, compare_positions), 0,
                                        np.where(np.greater(positions, compare_positions), -1, 1)
                                        )
-    positions = positions + velocity
+    positions += velocity
     # part 1
     if j == steps_part1:
         energy = np.sum(np.sum(np.abs(positions), axis=1) * np.sum(np.abs(velocity), axis=1))
@@ -29,7 +29,7 @@ while True:
     # get frequency of velocity as difference between to times where velocity is zero (beinning - x)
         if not repeatv[k] and np.all(velocity[:, k] == 0):
             repeatv[k] = j
-    if all(repeatv.values()) and j > steps_part1:
+    if all(repeatv.values()) and j >= steps_part1:
         break
 
 # velocity period is only half of complete period -> multiply with 2
