96037eaba97e06f91ec15a6093f276f226a1c280
HrRodan <m.mueller89@gmail.com>
2022-05-03 21:42:39 +0200

    day 18 part2

--- day18/day18.py
+++ day18/day18.py
@@ -47,6 +47,13 @@ links: Dict[str, Dict[str, Tuple[int, set]]] = defaultdict(dict)
 for k1, value1 in key_and_start.items():
     for k2, value2 in key_and_start.items():
         if k1 != k2:
+            # avoid calculating the same path twice
+            try:
+                links[value1][value2] = links[value2][value1]
+                continue
+            except KeyError:
+                pass
+            # continue if no path exists (different quadrant)
             try:
                 shortest_path = nx.shortest_path(G_spaces, k1, k2)
             except nx.exception.NetworkXNoPath:
@@ -57,24 +64,26 @@ for k1, value1 in key_and_start.items():
 
 cache = {}
 
-def walk(start_point: Set[str], needed_keys: set, length: int):
+
+def walk(start_point: Set[str], needed_keys: set):
     if not needed_keys:
-        return length
+        return 0
+    # tuple is faster than string join
     cache_tuple = (tuple(sorted(start_point)), tuple(needed_keys))
     if cache_tuple in cache:
-        return length + cache[cache_tuple]
+        return cache[cache_tuple]
     min_length = float("inf")
     for key in needed_keys:
         for s in start_point:
             link = links[s]
             if key in link:
                 shortest_path_length, doors_on_path = link[key]
-                if length + shortest_path_length < min_length and doors_on_path.isdisjoint(needed_keys):
-                    length_full = walk((start_point - {s}).union(key), needed_keys - {key}, length + shortest_path_length)
+                if shortest_path_length < min_length and doors_on_path.isdisjoint(needed_keys):
+                    length_full = shortest_path_length + walk((start_point - {s}).union(key), needed_keys - {key})
                     min_length = min(length_full, min_length)
-    cache[cache_tuple] = min_length - length
+    cache[cache_tuple] = min_length
     return min_length
 
 
-a = walk(set(start_points.values()), all_keys, 0)
+a = walk(set(start_points.values()), all_keys)
 print(a)
